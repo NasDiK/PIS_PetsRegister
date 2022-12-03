664b0f27efbe62c912cc980546baddc9f11d452e
romankuvshanov <kuvshanovr@gmail.com>
2022-12-03 16:23:00 +0500

    Добавил кнопки на форму мои домашние животные

--- WinFormsApplication/Forms/MainForm/AllAdvertisments/MyPets.Designer.cs
+++ WinFormsApplication/Forms/MainForm/AllAdvertisments/MyPets.Designer.cs
@@ -30,10 +30,13 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MyPets));
             this.panel1 = new System.Windows.Forms.Panel();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.addButton = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.addButton = new System.Windows.Forms.Button();
-            this.deleteButton = new System.Windows.Forms.Button();
+            this.buttonEdit = new System.Windows.Forms.Button();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.buttonSubmitAd = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
@@ -41,21 +44,48 @@
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.buttonSubmitAd);
+            this.panel1.Controls.Add(this.buttonOpen);
+            this.panel1.Controls.Add(this.buttonEdit);
             this.panel1.Controls.Add(this.deleteButton);
             this.panel1.Controls.Add(this.addButton);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(319, 54);
+            this.panel1.Size = new System.Drawing.Size(1031, 90);
             this.panel1.TabIndex = 0;
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.deleteButton.Location = new System.Drawing.Point(178, 20);
+            this.deleteButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(166, 60);
+            this.deleteButton.TabIndex = 1;
+            this.deleteButton.Text = "Удалить";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            // 
+            // addButton
+            // 
+            this.addButton.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.addButton.Location = new System.Drawing.Point(17, 20);
+            this.addButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(153, 60);
+            this.addButton.TabIndex = 0;
+            this.addButton.Text = "Добавить";
+            this.addButton.UseVisualStyleBackColor = true;
             // 
             // panel2
             // 
             this.panel2.Controls.Add(this.dataGridView1);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel2.Location = new System.Drawing.Point(0, 54);
+            this.panel2.Location = new System.Drawing.Point(0, 90);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(319, 316);
+            this.panel2.Size = new System.Drawing.Size(1031, 527);
             this.panel2.TabIndex = 1;
             // 
             // dataGridView1
@@ -63,40 +93,56 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
             this.dataGridView1.RowTemplate.Height = 25;
-            this.dataGridView1.Size = new System.Drawing.Size(319, 316);
+            this.dataGridView1.Size = new System.Drawing.Size(1031, 527);
             this.dataGridView1.TabIndex = 0;
             // 
-            // addButton
+            // buttonEdit
             // 
-            this.addButton.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.addButton.Location = new System.Drawing.Point(12, 12);
-            this.addButton.Name = "addButton";
-            this.addButton.Size = new System.Drawing.Size(31, 36);
-            this.addButton.TabIndex = 0;
-            this.addButton.Text = "+";
-            this.addButton.UseVisualStyleBackColor = true;
+            this.buttonEdit.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.buttonEdit.Location = new System.Drawing.Point(352, 20);
+            this.buttonEdit.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonEdit.Name = "buttonEdit";
+            this.buttonEdit.Size = new System.Drawing.Size(166, 60);
+            this.buttonEdit.TabIndex = 2;
+            this.buttonEdit.Text = "Изменить";
+            this.buttonEdit.UseVisualStyleBackColor = true;
             // 
-            // deleteButton
+            // buttonOpen
             // 
-            this.deleteButton.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.deleteButton.Location = new System.Drawing.Point(49, 12);
-            this.deleteButton.Name = "deleteButton";
-            this.deleteButton.Size = new System.Drawing.Size(200, 36);
-            this.deleteButton.TabIndex = 1;
-            this.deleteButton.Text = "(TODO удалить)";
-            this.deleteButton.UseVisualStyleBackColor = true;
+            this.buttonOpen.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.buttonOpen.Location = new System.Drawing.Point(526, 20);
+            this.buttonOpen.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(166, 60);
+            this.buttonOpen.TabIndex = 3;
+            this.buttonOpen.Text = "Открыть";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            // 
+            // buttonSubmitAd
+            // 
+            this.buttonSubmitAd.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.buttonSubmitAd.Location = new System.Drawing.Point(700, 20);
+            this.buttonSubmitAd.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonSubmitAd.Name = "buttonSubmitAd";
+            this.buttonSubmitAd.Size = new System.Drawing.Size(318, 60);
+            this.buttonSubmitAd.TabIndex = 4;
+            this.buttonSubmitAd.Text = "Подать объявление";
+            this.buttonSubmitAd.UseVisualStyleBackColor = true;
             // 
             // MyPets
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(319, 370);
+            this.ClientSize = new System.Drawing.Size(1031, 617);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "MyPets";
@@ -115,5 +161,8 @@
         private Button addButton;
         private DataGridView dataGridView1;
         private Button deleteButton;
+        private Button buttonSubmitAd;
+        private Button buttonOpen;
+        private Button buttonEdit;
     }
 }
